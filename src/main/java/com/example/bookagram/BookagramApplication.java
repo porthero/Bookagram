2c8733b954f5dcab648d7f5f5422ca90c11e9317
porthero <bddcks@gmail.com>
2023-04-30 18:47:33 +0200

    first commit

---
+++ src/main/java/com/example/bookagram/BookagramApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookagramApplication.class, args);
+	}
+
+}
